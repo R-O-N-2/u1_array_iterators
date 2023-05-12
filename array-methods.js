c3e75265703a595fc3c3c2b87088218c666ddb25
Ron <ron_rose@me.com>
2023-05-12 09:00:22 -0400

    updated notes

---
+++ array-methods.js
@@ -0,0 +1,34 @@
+//   ARRAY ITERATOR METHODS
+
+
+// forEach
+
+
+// const friends = ["Melissa", "Marc", "Andrew", "Nick"]
+
+// friends.forEach((friend)=> {
+//     console.log(`I have a friend named ${friend}`)
+// })
+
+
+
+// Map!
+
+// const nums =[1, 2, 3]
+
+// //function  using map to square numbers
+// const squared = nums.map((num)=> num * num)
+
+// console.log(squared)
+
+
+// REVIEW!!!!!!!
+// const instructors = ["Alex", "Stephanie", "Daniel"]
+
+// const yay = instructors.map((instructors)
+
+
+
+
+//      FILTER
+
